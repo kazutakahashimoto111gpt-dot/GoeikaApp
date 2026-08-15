f443eec57037ee594d6a66053b71319338bd510d
kazutaka-windows <kazutaka.hashimoto111gpt@gmail.com>
2026-08-15 19:52:34 +0900

    v1.0.2 画像変更に伴い座標点変更

--- notes.js
+++ notes.js
@@ -3,200 +3,200 @@ const notes = [
   {
     no: 1,
     name: "黄鐘（おうしき）",
-    xRatio: 0.3098,
-    yRatio: 0.5365,
+    xRatio: 0.3134,
+    yRatio: 0.5437,
     frequency: 220.000
   },
 
   {
     no: 2,
     name: "鸞鏡（らんげい）",
-    xRatio: 0.3081,
-    yRatio: 0.4594,
+    xRatio: 0.3134,
+    yRatio: 0.4670,
     frequency: 233.082
   },
 
   {
     no: 3,
     name: "盤渉（ばんしき）",
-    xRatio: 0.3385,
-    yRatio: 0.3888,
+    xRatio: 0.3416,
+    yRatio: 0.3944,
     frequency: 246.942
   },
 
   {
     no: 4,
     name: "神仙（しんせん）",
-    xRatio: 0.3878,
-    yRatio: 0.336,
+    xRatio: 0.3924,
+    yRatio: 0.3416,
     frequency: 261.626
   },
 
   {
     no: 5,
     name: "一",
-    xRatio: 0.4478,
-    yRatio: 0.3084,
+    xRatio: 0.4547,
+    yRatio: 0.3135,
     frequency: 277.183
   },
 
   {
     no: 6,
     name: "一半",
-    xRatio: 0.5472,
-    yRatio: 0.211,
+    xRatio: 0.5536,
+    yRatio: 0.2137,
     frequency: 293.665
   },
 
   {
     no: 7,
     name: "二",
-    xRatio: 0.6557,
-    yRatio: 0.2597,
+    xRatio: 0.6608,
+    yRatio: 0.2591,
     frequency: 311.127
   },
 
   {
     no: 8,
     name: "二半",
-    xRatio: 0.7329,
-    yRatio: 0.3336,
+    xRatio: 0.7406,
+    yRatio: 0.3399,
     frequency: 329.628
   },
 
   {
     no: 9,
     name: "三",
-    xRatio: 0.7699,
-    yRatio: 0.4375,
+    xRatio: 0.7805,
+    yRatio: 0.4455,
     frequency: 349.228
   },
 
   {
     no: 10,
     name: "三半",
-    xRatio: 0.7724,
-    yRatio: 0.5544,
+    xRatio: 0.7814,
+    yRatio: 0.5635,
     frequency: 369.994
   },
 
   {
     no: 11,
     name: "四",
-    xRatio: 0.7354,
-    yRatio: 0.6477,
+    xRatio: 0.7431,
+    yRatio: 0.6601,
     frequency: 391.995
   },
 
   {
     no: 12,
     name: "四半",
-    xRatio: 0.6549,
-    yRatio: 0.7297,
+    xRatio: 0.6600,
+    yRatio: 0.7426,
     frequency: 415.305
   },
 
   {
     no: 13,
     name: "五",
-    xRatio: 0.5538,
-    yRatio: 0.7808,
+    xRatio: 0.5578,
+    yRatio: 0.7904,
     frequency: 440.000
   },
 
   {
     no: 14,
     name: "五半",
-    xRatio: 0.4339,
-    yRatio: 0.7768,
+    xRatio: 0.4389,
+    yRatio: 0.7921,
     frequency: 466.164
   },
 
   {
     no: 15,
     name: "六",
-    xRatio: 0.3328,
-    yRatio: 0.733,
+    xRatio: 0.3400,
+    yRatio: 0.7483,
     frequency: 493.883
   },
 
   {
     no: 16,
     name: "六半",
-    xRatio: 0.2547,
-    yRatio: 0.6583,
+    xRatio: 0.2585,
+    yRatio: 0.6650,
     frequency: 523.251
   },
 
   {
     no: 17,
     name: "七",
-    xRatio: 0.2136,
-    yRatio: 0.5308,
+    xRatio: 0.2170,
+    yRatio: 0.5396,
     frequency: 554.365
   },
 
   {
     no: 18,
     name: "七半",
-    xRatio: 0.1002,
-    yRatio: 0.3888,
+    xRatio: 0.1031,
+    yRatio: 0.3969,
     frequency: 587.330
   },
 
   {
     no: 19,
     name: "八",
-    xRatio: 0.1619,
-    yRatio: 0.2695,
+    xRatio: 0.1596,
+    yRatio: 0.2756,
     frequency: 622.254
   },
 
   {
     no: 20,
     name: "八半",
-    xRatio: 0.2753,
-    yRatio: 0.1494,
+    xRatio: 0.2768,
+    yRatio: 0.1543,
     frequency: 659.255
   },
 
   {
     no: 21,
     name: "九",
-    xRatio: 0.41,
-    yRatio: 0.0893,
+    xRatio: 0.4165,
+    yRatio: 0.0908,
     frequency: 698.456
   },
 
   {
     no: 22,
     name: "九半",
-    xRatio: 0.5735,
-    yRatio: 0.0909,
+    xRatio: 0.5794,
+    yRatio: 0.0899,
     frequency: 739.989
   },
 
   {
     no: 23,
     name: "十",
-    xRatio: 0.7058,
-    yRatio: 0.1461,
+    xRatio: 0.7182,
+    yRatio: 0.1518,
     frequency: 783.991
   },
 
   {
     no: 24,
     name: "十半",
-    xRatio: 0.825,
-    yRatio: 0.2687,
+    xRatio: 0.8362,
+    yRatio: 0.2715,
     frequency: 830.609
   },
 
   {
     no: 25,
     name: "十一",
-    xRatio: 0.8858,
-    yRatio: 0.4075,
+    xRatio: 0.8953,
+    yRatio: 0.4158,
     frequency: 880.000
   }
 
